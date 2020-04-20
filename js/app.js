7059d5a5458b5f9e78ba8dffeeadc2c89c029b12
mikareich <mikareich0@gmail.com>
2020-04-20 12:08:38 +0200

    add pwa

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ const eOB = document.querySelector(".expandable-operators-block"); // long: expa
 const eOB_trigger = eOB.querySelector(".trigger");
 
 eOB_trigger.addEventListener("click", () => eOB.classList.toggle("expand"));
+
+import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate";
+
+const el = document.createElement("pwa-update");
+document.body.appendChild(el);
